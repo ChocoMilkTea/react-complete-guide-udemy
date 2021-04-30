bcf7cada80e47a8207dcef5ffd87160d25d9c7ee
ChocoMilkTea <hongjun87@gmail.com>
2021-05-01 00:18:01 +0800

    add composition and extract out common css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expense from './components/Expense';
 
 function App() {
   const expenses = [
-    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) }
+    { title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 8, 14) },
+    { title: 'New TV', amount: 799.49, date: new Date(2021, 3, 12) },
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 3, 28) },
+    { title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 6, 12) }
   ]
 
   return (
     <div>
-      <h2>Let's get started</h2>
-
-      <ExpenseItem
-        title={ expenses[0].title }
-        amount={expenses[0].amount }
-        date={ expenses[0].date }
-      />
-      <ExpenseItem title={ expenses[1].title } amount={expenses[1].amount } date={ expenses[1].date }/>
-      <ExpenseItem title={ expenses[2].title } amount={expenses[2].amount } date={ expenses[2].date }/>
-      <ExpenseItem title={ expenses[3].title } amount={expenses[3].amount } date={ expenses[3].date }/>
+      <Expense expenses={ expenses }/>
     </div>
   );
 }
---
+++ src/components/Expense.js
@@ -0,0 +1,29 @@
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+import './Expense.css';
+
+function Expense(props) {
+  const expenseItems = []
+
+  for (const [index, key] of props.expenses.entries()) {
+    expenseItems.push(
+      <ExpenseItem
+        title={key.title}
+        amount={key.amount}
+        date={key.date}
+      />
+    )
+  }
+
+  return (
+    <div>
+      <h2>Let's get started</h2>
+
+      <Card className="expenses">
+        {expenseItems}
+      </Card>
+    </div>
+  );
+}
+
+export default Expense;
